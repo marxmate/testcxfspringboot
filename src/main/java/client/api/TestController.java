a1c55bbd40ba963f193105514b20edfc96778956
Máté Marx <mate.marx@dpdgroup.com>
2018-12-06 12:30:54 +0100

    interface - implementation separated

--- src/main/java/client/api/TestController.java
+++ src/main/java/client/api/TestController.java
@@ -1,24 +1,13 @@
 package client.api;
 
-import client.service.TestService;
 import model.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/test")
-public class TestController {
-
-    private TestService testService;
-
-    @Autowired
-    public TestController(TestService testService) {
-        this.testService = testService;
-    }
+public interface TestController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public Test getTest() {
-        return testService.getClient().getTest("nameIn", 6);
-    }
+    Test getTest();
 }
---
+++ src/main/java/client/api/TestControllerImpl.java
@@ -0,0 +1,22 @@
+package client.api;
+
+import client.service.TestService;
+import model.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestControllerImpl implements TestController {
+
+    private TestService testService;
+
+    @Autowired
+    public TestControllerImpl(TestService testService) {
+        this.testService = testService;
+    }
+
+    @Override
+    public Test getTest() {
+        return testService.getClient().getTest("nameIn", 6);
+    }
+}
