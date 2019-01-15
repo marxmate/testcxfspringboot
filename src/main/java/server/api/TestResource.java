5049cddf47c8d2c7f633e358dc7c519eecd2a659
Máté Marx <mate.marx@dpdgroup.com>
2019-01-15 22:36:10 +0100

    setup with openApi feature

--- src/main/java/server/api/TestResource.java
+++ src/main/java/server/api/TestResource.java
@@ -1,7 +1,7 @@
 package server.api;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import model.Test;
-import org.apache.cxf.security.claims.authorization.Claim;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -10,6 +10,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import static server.api.config.SwaggerConfig.BEARER_AUTH;
+
+@SecurityRequirement(name = BEARER_AUTH)
 @Path("/test")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -17,6 +20,5 @@ public interface TestResource {
 
     @GET
     @Path("/getTest")
-    @Claim("admin")
     Test getTestObject(@QueryParam("name") String name, @QueryParam("int") int integerSmall);
 }
--- src/main/java/server/api/config/SwaggerConfig.java
+++ src/main/java/server/api/config/SwaggerConfig.java
@@ -1,31 +1,43 @@
 package server.api.config;
 
-import io.swagger.config.ScannerFactory;
-import io.swagger.jaxrs.config.BeanConfig;
-import io.swagger.models.Swagger;
-import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import model.Test;
+import org.apache.cxf.jaxrs.openapi.OpenApiFeature;
+import org.apache.cxf.jaxrs.swagger.ui.SwaggerUiConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
+
+import javax.ws.rs.QueryParam;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    @ConfigurationProperties("swagger")
-    public Swagger2Feature swagger() {
-        return new ExtendedSwagger2Feature();
-    }
+    public static final String BEARER_AUTH = "bearerAuth";
 
     @Bean
-    @DependsOn("jaxRsServer")
-    public ServletContextInitializer initializer() {
-        return servletContext -> {
-            BeanConfig scanner = (BeanConfig) ScannerFactory.getScanner();
-            Swagger swagger = scanner.getSwagger();
-            servletContext.setAttribute("swagger", swagger);
-        };
+    @ConfigurationProperties("swagger")
+    public OpenApiFeature swagger() {
+        OpenApiFeature openApiFeature = new OpenApiFeature();
+        openApiFeature.setTitle("Backend Application");
+        openApiFeature.setDescription("Swagger documentation of Backend Application REST services");
+        openApiFeature.setSecurityDefinitions(Collections.singletonMap(BEARER_AUTH, new SecurityScheme()
+                .in(SecurityScheme.In.HEADER)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .name("Authorization")
+                .bearerFormat("JWT")));
+        openApiFeature.setScan(true);
+        Set<String> packages = new HashSet<>();
+        packages.add("server.api");
+        openApiFeature.setResourcePackages(packages);
+        openApiFeature.setReadAllResources(true);
+        openApiFeature.setSwaggerUiConfig(
+                new SwaggerUiConfig()
+                        .url("/rest/openapi.json"));
+        return openApiFeature;
     }
 }
